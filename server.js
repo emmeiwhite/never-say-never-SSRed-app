ab6e8b804f79b3d7be197d5b677f82eeda21692a
emmeiwhite <emmeiwhite@gmail.com>
2024-09-29 00:05:05 +0530

    Server with 2 basic routes!

--- server.js
+++ server.js
@@ -3,7 +3,17 @@ const { parse } = require('url')
 
 const server = createServer((req, res) => {
   // Route-1 Homepage!
-  res.end('Hello World!')
+  // res.end('Hello World!')
+
+  const pathName = parse(req.url, true).pathname
+
+  if (pathName === '/') {
+    res.end('Hello World! - Route-1')
+  } else if (pathName === '/test') {
+    res.end('TEST FILE - Route-2')
+  } else {
+    res.end('The URL cannot be found')
+  }
 })
 
 server.listen(8000, () => {
